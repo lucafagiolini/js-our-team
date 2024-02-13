bd213205635419aad34cbe14eedacd76e17eea34
Luca <lucafagiolini@live.it>
2024-02-13 14:22:05 +0100

    array of objects created

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+let teamMembers = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        img: "img/wayne-barnett-founder-ceo.jpg",
+    },
+
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        img: "img/angela-caroll-chief-editor.jpg",
+    },
+
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        img: "img/walter-gordon-office-manager.jpg",
+    },
+
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        img: "img/angela-lopez-social-media-manager.jpg",
+    },
+
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        img: "img/scott-estrada-developer.jpg",
+    }
+]
